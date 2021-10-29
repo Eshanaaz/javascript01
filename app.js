d21a0d0cdc77679a618bd0afe6e0c15671f84674
Esha Naz <eshanaz76@gmail.com>
2021-10-30 01:00:47 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,258 @@
+                        //popup
+// alert("EshaNaz")
+
+
+           //string
+
+// var firstName="Esha"
+// var lastName="Naz"
+// var fullName= ( firstName + lastName)
+// alert(fullName)
+
+
+            //Numeric
+ 
+//   var num1=20 
+//   var num2=10
+//   var tot= num1+num2
+//   alert(tot)         
+
+
+
+                                     //BASIC APPARATUS
+
+//      //PLUS                                
+ 
+//  var a=10
+//  var b=40
+//  var c=(a+b)
+ 
+//  alert(c)
+
+  //SUBTRACT                                
+ 
+// var a=10
+//  var b=40
+// var c=(a-b)
+ 
+// alert(c)
+
+
+//   //MULTIPLY                                
+ 
+//   var a=10
+//   var b=40
+//  var c=(a*b)
+  
+//  alert(c)
+ 
+
+  //SUBTRACT                                
+ 
+//   var a=10
+//   var b=40
+//  var c=(a/b)
+  
+//  alert(c)
+ 
+
+
+            //MODULE                                
+ 
+//   var a=10
+//   var b=40
+//  var c=(a%b)
+  
+//  alert(c)
+
+
+
+                                 ////SAME VARIABLE NAME
+ 
+//  var name= "ESHA";
+//  var name="Naz";
+//  alert(name);  
+
+                                     ///CONCTENATION
+
+// var firstName="Esha";
+// var rollNum=01;
+// var bio= firstName + rollNum
+
+// alert(bio)
+
+
+  //CONCAT 1
+
+// var firstName=30;
+// var rollNum=01;
+// var bio= firstName + rollNum
+
+// alert(bio)
+
+
+  //CONCAT 2
+
+
+// var firstName="10";
+// var rollNum="10";
+// var bio= firstName + rollNum
+
+// alert(bio)
+
+
+  //CONCAT 3
+
+
+
+// var firstName="10";
+// var rollNum="10";
+// var bio= firstName - rollNum
+
+// alert(bio)
+
+
+
+  //CONCAT 4
+
+
+// var firstName="10";
+// var rollNum="10";
+// var bio= firstName / rollNum
+
+// alert(bio)
+
+
+
+  //CONCAT 5
+
+
+
+// var firstName="10";
+// var rollNum="10";
+// var bio= firstName * rollNum
+
+// alert(bio)
+
+
+                                      ////BODMAS
+
+
+// var a= (3-1) +3 -9
+// alert(a) ;
+
+
+
+
+                            ///////////POST INCREEMENT
+
+
+//  var a=5
+
+//       //6
+//  var b= a++ + a++   + a
+//      // 5   + 6     + 7 
+
+ 
+//  alert(b);
+
+                                //PRE INCREEMENT
+
+
+//  var a = 10 
+
+//  var result = a++   +  ++ a
+//             //   10    +   12
+
+//  alert(result)
+
+
+
+
+//  var a = 10;  
+//         // 11
+//  var result = a++   +    ++a    +    a    +     a++
+//         //    10             12      12         
+// alert(result)
+ 
+
+
+                      /////INCREEMENT AND DECREEMENT
+
+
+
+
+// var a = 20;
+//             //21
+//  var result = a++    -   --a     +   --a   +   a++   +   a   +   a   +    ++a 
+//         //    20         20          19        19        19      19      20
+
+//          //    19 + 19  +  20 + 20 + 21 
+//  alert(result)
+
+
+
+                  ///////////////ASSIGNMENT
+
+
+
+//  var userName = prompt("Enter your name", "Your name");
+//  document.write( "NAME:" + " " +   " " +  userName +"<br>" )              
+
+ 
+ 
+//  var rollNum= prompt("Enter roll num","Enter roll num")
+//  document.write("Roll Number:" + " " +   " " + rollNum + "<br>")
+
+
+
+//  var institueName= prompt("Enter Instituion name", "Your Institution")
+// document.write( "Instituion Name:"+ " " +   " "  + institueName +"<br>"+"<br>")
+
+// var phy=prompt("BIOPHYSICS MARKS","biophysics marks")
+// document.write("BIOPHYSICS:" + " " +   " "  + phy + "<br>")
+
+// var biochem=prompt("BIOCHEMISTRY MARKS","biochemistry marks")
+// document.write("BIOCHEMISTRY:"+ " " +   " " + biochem + "<br>")
+
+// var micro=prompt("MICROBIOLOGY MARKS","microbiology marks")
+// document.write("MICROBIOLOGY:" + " " +   " " + micro + "<br>")
+
+// var physio=prompt("PHYSIOLOGY MARKS","physiology marks")
+// document.write("PHYSIOLOGY:"+ " " +   " " + physio + "<br>")
+
+// var states=prompt("BIOSTATISTICS MARKS","biostatistics marks")
+// document.write("BIOSTATISTICS:"+ " " +   " " + states+ "<br>"+"<br>")
+
+
+
+
+
+
+
+// var obtainedmarks = parseInt(phy) + parseInt(biochem) + parseInt(micro) + parseInt(physio) + parseInt(states);
+// var percentage = obtainedmarks / 500 * 100;
+
+// document.write("Your Total Marks is:" + obtainedmarks + "<br/>");
+// document.write("Your Percentage is:" + percentage + "%");
+
+
+
+
+
+
+
+
+
+
+// // percetage = obt / total * 100
+
+
+
+
+
+
+
+
+
+
